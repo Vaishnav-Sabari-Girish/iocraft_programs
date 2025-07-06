6f0f6222862c7ad1456b92156e62fe26ebd8c86f
Vaishnav-Sabari-Girish <forgamesonly684@gmail.com>
2025-07-06 22:41:12 +0530

    Added counter

---
+++ counter/src/main.rs
@@ -0,0 +1,24 @@
+use iocraft::{components, prelude::*};
+use std::time::Duration;
+
+#[component]
+fn Counter(mut hooks: Hooks) -> impl Into<AnyElement<'static>> {
+    let mut count = hooks.use_state(|| 0);
+
+    hooks.use_future(async move {
+        loop {
+            smol::Timer::after(Duration::from_millis(100)).await;
+            count += 1;
+        }
+    });
+
+    element! {
+        Text(color: Color::Blue, content: format!("Counter: {}", count))
+    }
+}
+
+fn main(){
+    smol::block_on(
+        element!(Counter).render_loop()
+    ).unwrap();
+}
