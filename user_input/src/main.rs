3332d2d80b8e09186386e321e51215627b77c628
Vaishnav-Sabari-Girish <forgamesonly684@gmail.com>
2025-07-10 14:37:54 +0530

    Added user input proper

--- user_input/src/main.rs
+++ user_input/src/main.rs
@@ -29,21 +29,34 @@ fn NameInput(props: &NameInputProps) -> impl Into<AnyElement<'static>> {
                     on_change: move |new_value| value.set(new_value),
                 )
             }
-        }
+            }
     }
 }
 
 #[component]
 fn NameForm(mut hooks: Hooks) -> impl Into<AnyElement<'static>> {
-    let mut system = hooks.use_context_mut::<SystemContext>();
     let name = hooks.use_state(|| "".to_string());
-    let has_focus = hooks.use_state(|| true);
+    let age = hooks.use_state(|| "".to_string());
+    let mut current_field = hooks.use_state(|| 0);   //0 = name, 1 = age
     let mut should_submit = hooks.use_state(|| false);
+    let mut submitted_name = hooks.use_state(|| "".to_string());
+    let mut submitted_age = hooks.use_state(|| 0);
+
 
     hooks.use_terminal_events(move |event| match event {
         TerminalEvent::Key(KeyEvent { code, kind, .. }) if kind != KeyEventKind::Release => {
             match code {
-                KeyCode::Enter => should_submit.set(true),
+                KeyCode::Tab => {
+                    current_field.set((current_field.get() + 1) % 2);
+                },
+                KeyCode::Enter => {
+                    if current_field.get() == 0 {
+                        current_field.set(1);
+                    }
+                    else {
+                        should_submit.set(true);
+                    }
+                },
                 _ => {}
             }
         }
@@ -51,8 +64,23 @@ fn NameForm(mut hooks: Hooks) -> impl Into<AnyElement<'static>> {
     });
 
     if should_submit.get() {
-        system.exit();
-        element! (View)
+        if submitted_name.to_string().is_empty() {
+            submitted_name.set(name.to_string());
+        }
+        if submitted_age.get() == 0 {
+            submitted_age.set(age.to_string().parse().expect("Not a valid number"));
+        }
+
+        element! {
+            View (
+                flex_direction: FlexDirection::Column,
+                align_items: AlignItems::Center,
+                margin: 2
+            ) {
+                Text(content: format!("Hello, {}", submitted_name.to_string()), color: Color::Green)
+                Text(content: format!("You are {} years old", submitted_age), color: Color::Blue)
+            }
+        }
     }
     else {
         element! {
@@ -61,13 +89,28 @@ fn NameForm(mut hooks: Hooks) -> impl Into<AnyElement<'static>> {
                 align_items: AlignItems::Center,
                 margin: 2
             ) {
-                View (margin_bottom : 1) {
+                View (
+                    flex_direction: FlexDirection::Row,
+                    align_items: AlignItems::Center,
+                    margin_bottom: 1
+                ) {
                     Text(content: "Enter your Name : ", color: Color::White)
+                    NameInput(value: name, has_focus: current_field.get() == 0)
                 }
-                NameInput(value: name, has_focus: has_focus.get())
+
+                View (
+                    flex_direction: FlexDirection::Row,
+                    align_items: AlignItems::Center,
+                    margin_bottom: 1,
+                ) {
+                    Text(content: "Enter you age: ", color: Color::White)
+                    NameInput(value: age, has_focus: current_field.get() == 1)
+                }
+
                 View(margin_top: 1) {
                     Text(content: "Press Enter to submit", color: Color::Grey)
                 }
+
             }
         }
     }
